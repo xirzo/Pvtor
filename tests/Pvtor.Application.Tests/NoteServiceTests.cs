5849442db332ffcab48ff49932a40bc09f6c9218
xirzo <easttem@gmail.com>
2026-01-15 18:20:15 +0300

    test: create note

---
+++ tests/Pvtor.Application.Tests/NoteServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Pvtor.Application.Abstractions.Persistence;
+using Pvtor.Application.Abstractions.Persistence.Repositories;
+using Pvtor.Application.Contracts.Notes.Operations;
+using Pvtor.Application.Services;
+using Pvtor.Domain.Notes;
+
+namespace Pvtor.Domain.Tests;
+
+public class NoteServiceTests
+{
+    [Fact]
+    public void Create_ValidRequest_ReturnsNote()
+    {
+        // Arrange
+        IPersistanceContext persitenceContext = Substitute.For<IPersistanceContext>();
+        INoteRepository noteRepository = Substitute.For<INoteRepository>();
+        persitenceContext.NoteRepository.Returns(noteRepository);
+
+        noteRepository.Add(Arg.Any<Note>())
+            .Returns(info =>
+            {
+                Note addedNode = info.Arg<Note>();
+                return addedNode;
+            });
+
+        var noteService = new NoteService(persitenceContext);
+        const string content = "Pvtor";
+
+        // Act
+        CreateNote.Response response = noteService.CreateNote(new CreateNote.Request(content));
+
+        // Assert
+        Assert.IsType<CreateNote.Response.Success>(response);
+        var success = response as CreateNote.Response.Success;
+        Assert.NotNull(success);
+        Assert.Equal(content, success.Note.Content);
+    }
+}
